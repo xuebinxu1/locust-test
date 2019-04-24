b49ac23f708a8cb30d03ec66063753c27ce4b344
chengjin123 <chengjin.li@ecreditpal.com>
2019-04-24 18:36:38 +0800

    进件流程：银行卡认证、审核通过（pass、reject）、还款

---
+++ script/service/repayment_operate_service.py
@@ -0,0 +1,92 @@
+from utils import convert, models
+from datetime import datetime
+from utils.my_snowflake import my_snow
+import random
+
+
+def get_contract_no():
+    year = datetime.now().year
+    month = datetime.now().month
+    day = datetime.now().day
+    num = random.randint(0, 999999999999999)
+    return str(year) + str(month) + str(day) + str(num)
+
+
+def get_transaction(contract_no, bid, event_id, status):
+    transaction = models.Transaction()
+    transaction.id = my_snow().get_next_id()
+    transaction.company_id = bid.company_id
+    transaction.contract_no = contract_no
+    transaction.type = 2
+    transaction.scene = 2
+    transaction.status = status
+    transaction.amount = bid.amount
+    transaction.customer_name = datetime.now()
+    transaction.mobile = bid.mobile
+    transaction.bid_id = bid.id
+    transaction.bid_contract_no = bid.contract_no
+    transaction.payment_channel = "富友"
+    transaction.payment_channel_id = 2
+    transaction.event_id = event_id
+    transaction.channel_id = bid.channel_id
+    transaction.transaction_time = datetime.now()
+    transaction.comment = None
+    return transaction
+
+
+def operate(event_id, bid_id):
+    """
+    还款
+    更新bid表、插入bid_workflow记录
+    :return:
+    """
+    update = {'status': 7}
+    bid = convert.query(models.Bid, id=bid_id).first()
+    contract_no = get_contract_no()
+    # 插入bid_workflow记录
+    bid_workflow = models.BidWorkflow()
+    bid_workflow.id = my_snow().get_next_id()
+    bid_workflow.company_id = bid.company_id
+    bid_workflow.bid_id = bid.id
+    bid_workflow.user_id = bid.user_id
+    bid_workflow.last_status = 3
+    bid_workflow.current_status = 7
+    bid_workflow.operator_id = 0
+    bid_workflow.operating_time = datetime.now()
+    bid_workflow.action = "还款"
+    bid_workflow.suggestion = None
+    bid_workflow.channel_id = bid.channel_id
+    # 插入current_transaction记录
+    current_transaction = models.CurrentTransaction()
+    current_transaction.id = my_snow().get_next_id()
+    current_transaction.company_id = bid.company_id
+    current_transaction.contract_no = contract_no
+    current_transaction.type = 2
+    current_transaction.scene = 2
+    current_transaction.status = 1
+    current_transaction.amount = bid.amount
+    current_transaction.customer_name = datetime.now()
+    current_transaction.mobile = bid.mobile
+    current_transaction.bid_id = bid.id
+    current_transaction.bid_contract_no = bid.contract_no
+    current_transaction.payment_channel = "富友"
+    current_transaction.payment_channel_id = 2
+    current_transaction.event_id = event_id
+    current_transaction.channel_id = bid.channel_id
+    current_transaction.transaction_time = datetime.now()
+    current_transaction.comment = None
+    transaction_list = list()
+    # 插入transaction记录初始化
+    transaction_zero = get_transaction(contract_no, bid, event_id, 0)
+    transaction_list.append(transaction_zero)
+    # 插入transaction记录成功
+    transaction = get_transaction(contract_no, bid, event_id, 1)
+    transaction_list.append(transaction)
+    convert.add_all(transaction_list)
+    # 插入current_transaction记录
+    convert.add_one(current_transaction)
+    # 更新bid表
+    convert.update_table(models.Bid, update, id=bid_id)
+    return convert.add_one(bid_workflow)
+
+# operate('1_0_15180279540_1550665419452', 9549969456)
---
+++ script/service/user_certification_service.py
@@ -0,0 +1,114 @@
+from utils.my_snowflake import my_snow
+from utils import convert, models
+
+
+def select():
+    """
+    测试类
+    :return:
+    """
+    dict_params = dict()
+    dict_params['mobile'] = 18857119819
+    dict_params['company_id'] = 1
+    record_list = convert.query(models.UserCertification, mobile=18857119819).all()
+    print(len(record_list))
+    print(record_list[0].status)
+
+
+# select()
+
+def insert_autonym(mobile, company_id):
+    """
+    0实名认证
+    user_certification表造数据
+    :return:
+    """
+    user = convert.query(models.User, mobile=mobile, company_id=company_id).first()
+    print(user)
+    user_certification = models.UserCertification()
+    user_certification.id = my_snow().get_next_id()
+    user_certification.channel_id = user.register_channel_id
+    user_certification.status = 1  # 成功
+    user_certification.succeed_flag = 1  # 成功
+    user_certification.type = 0  # 0:实名认证，1运营商认证，3银行卡认
+    user_certification.expiration_period = 7  # 7实名认证，30运营商认证，-1银行卡认
+    user_certification.mobile = mobile
+    user_certification.company_id = company_id
+
+    return convert.add_one(user_certification)
+
+
+# insert_autonym(15180279540, 1)
+
+
+def insert_operator(mobile, company_id):
+    """
+    1运营商认证
+    user_certification表造数据
+    :return:
+    """
+    user = convert.query(models.User, mobile=mobile, company_id=company_id).first()
+    print(user)
+    user_certification = models.UserCertification()
+    user_certification.id = my_snow().get_next_id()
+    user_certification.channel_id = user.register_channel_id
+    user_certification.status = 1  # 成功
+    user_certification.succeed_flag = 1  # 成功
+    user_certification.type = 1  # 0:实名认证，1运营商认证，3银行卡认
+    user_certification.expiration_period = 30  # 7实名认证，30运营商认证，-1银行卡认
+    user_certification.mobile = mobile
+    user_certification.company_id = company_id
+    return convert.add_one(user_certification)
+
+
+# insert_operator(15180279540, 1)
+
+
+def insert_bankcard(mobile, company_id):
+    """
+    3银行卡认证
+    user_certification表造数据
+    :return:
+    """
+    user = convert.query(models.User, mobile=mobile, company_id=company_id).first()
+    print(user)
+    user_certification = models.UserCertification()
+    user_certification.id = my_snow().get_next_id()
+    user_certification.channel_id = user.register_channel_id
+    user_certification.status = 1  # 成功
+    user_certification.succeed_flag = 1  # 成功
+    user_certification.type = 3  # 0:实名认证，1运营商认证，3银行卡认
+    user_certification.expiration_period = -1  # 7实名认证，30运营商认证，-1银行卡认
+    user_certification.mobile = mobile
+    user_certification.company_id = company_id
+    return convert.add_one(user_certification)
+
+
+# insert_bankcard(15180279540, 1)
+
+
+def update_cert_item(mobile, company_id, cert_item_list):
+    """
+    更新user_detail的认证项
+    user_detail表造数据
+    :return:
+    """
+    update = {'cert_item': ",".join(cert_item_list)}
+
+    return convert.update_table(models.UserDetail, update, mobile=mobile, company_id=company_id)
+
+
+# update_cert_item(15180279540, 1, ['0', '1'])
+
+
+def update_cert_item_of_user_apply_info(mobile, company_id, cert_item_list):
+    """
+    更新user_apply_info的认证项
+    user_apply_info表造数据
+    :return:
+    """
+    update = {'cert_item': ",".join(cert_item_list)}
+
+    return convert.update_table(models.UserApplyInfo, update, mobile=mobile, company_id=company_id)
+
+# update_cert_item_of_user_apply_info(15180279540, 1, ['0', '1', '3'])
